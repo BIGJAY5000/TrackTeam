2d25cd1aff5bb751e2418b3f092f058faf264f02
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-02-23 09:44:20 -0500

    gbb

--- src/Thrower.java
+++ src/Thrower.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Thrower {
+public class Thrower extends Athlete{
+    //attributes
+
+    private double shotPR;
+    private double discusPR;
+
+    //constructors
+    public Thrower(String firstName, String lastName, int gradeLevel, int pointsEarned, double shotPR, double discusPR){
+        super(firstName, lastName, gradeLevel, pointsEarned);
+        addEvent("Shot Put");
+        addEvent("Discus");
+        setDiscusPR(discusPR);
+        setShotPR(shotPR);
+    }
+    //mutators
+
+    public void setShotPR(double shotPR) {
+        if (shotPR >= 0 && shotPR <= 80) {
+            this.shotPR = shotPR;
+        } else {
+            this.shotPR = 0;
+        }
+    }
+    public void setDiscusPR(double discusPR) {
+        if (discusPR >= 0 && discusPR <= 240) {
+            this.discusPR = discusPR;
+        } else {
+            this.discusPR = 0;
+        }
+    }
+    //accessors
+    public double getDiscusPR() {
+        return discusPR;
+    }
+
+    public double getShotPR() {
+        return shotPR;
+    }
+
+    public String toString(){
+        return super.toString() + "\n Shot Put PR (ft)" + getShotPR() + "\nDiscus PR (ft)" + getDiscusPR();
+    }
 }
--- src/TrackTeam.java
+++ src/TrackTeam.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class TrackTeam {
+import java.util.ArrayList;
+public class TrackTeam {
+    public static void main(String[] args) {
+        //create coach object
+        ArrayList <Coach> coaches = new ArrayList<Coach>();
+        ArrayList <Thrower> throwers = new ArrayList<Thrower>();
+
+
+
+        Coach c1 = new Coach("Emory", "George", "Throws", false);
+        Coach c2 = new Coach("John", "Jarvis", "Distance", true);
+
+        coaches.add(c1);
+        coaches.add(c2);
+        System.out.println("Coaches");
+        for (Coach c: coaches) {
+            System.out.println(c);
+            System.out.println();
+        }
+
+        Thrower t1 = new Thrower("Jason", "Ocampo", 12, 0, 31, 97);
+        Thrower t2 = new Thrower("Dalton", "Sali", 12, 0, 27, 86);
+
+        throwers.add(t1);
+        throwers.add(t2);
+        System.out.println("Throwers");
+        for (Thrower t: throwers) {
+            System.out.println(t);
+            System.out.println();
+        }
+
+        t1.setShotPR(36);
+        t2.setDiscusPR(105);
+        for (Thrower t: throwers) {
+            System.out.println(t);
+            System.out.println();
+        }
+
+//        Athlete p1 = new Athlete("Emory", "George", 9, 0);
+//        System.out.println(c1);
+//        System.out.println();
+//        System.out.println(c2);
+//        p1.addEvent("Shot Put");
+//        p1.addEvent("Discus");
+//        p1.addPoints(20);
+//        System.out.println();
+
+//        //just print name
+//        System.out.println(c1.getFirstName());
+//        System.out.println();
+    }
 }
